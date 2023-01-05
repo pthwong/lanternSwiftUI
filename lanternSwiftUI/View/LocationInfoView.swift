11fa5882768353f6f5437fc96dcb2298d406ef16
thwong221 <tszhim.wong1010@yahoo.com>
2023-01-05 21:55:17 +0800

    Location Information View Added

---
+++ lanternSwiftUI/View/LocationInfoView.swift
@@ -0,0 +1,20 @@
+//
+//  LocationInfoView.swift
+//  lanternSwiftUI
+//
+//  Created by WONG TSZ HIM on 5/1/2023.
+//
+
+import SwiftUI
+
+struct LocationInfoView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct LocationInfoView_Previews: PreviewProvider {
+    static var previews: some View {
+        LocationInfoView()
+    }
+}
--- lanternSwiftUI/View/LocationListView.swift
+++ lanternSwiftUI/View/LocationListView.swift
@@ -10,11 +10,15 @@ import SwiftUI
 struct LocationListView: View {
     
     var body: some View {
-        List(lanternLocation) { location in
-            VStack(alignment: .leading) {
-                    Text(location.name).font(.title3).bold()
-                    Text(location.address).opacity(0.6)
-                    Text(location.description).font(.caption)
+        NavigationView {
+            List(lanternLocation) { location in
+                NavigationLink(destination: LocationInfoView()) {
+                    VStack(alignment: .leading) {
+                        Text(location.name).font(.title3).bold()
+                        Text(location.address).opacity(0.6)
+                        Text(location.description).font(.caption)
+                    }
+                }
             }
         }
     }
