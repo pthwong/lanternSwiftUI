64620e838b0959499f389348763002f1c6643006
thwong221 <tszhim.wong1010@yahoo.com>
2023-01-05 21:23:14 +0800

    Rearrangement of Data Model

--- lanternSwiftUI/Model/AnnotatedItem.swift
+++ lanternSwiftUI/Model/AnnotatedItem.swift
@@ -8,23 +8,16 @@
 import MapKit
 import Foundation
 
-struct AnnotatedItem : Identifiable, Decodable {
+struct AnnotatedItem : Decodable, Identifiable {
     var id: Int
     let name : String
     var description: String
     var address: String
     var location: String
     var district: String
-    
-    //var coordinates : CLLocationCoordinate2D
-    
-    var coordinate : Coordinate
-    var locationCoordinate: CLLocationCoordinate2D{
-        CLLocationCoordinate2D(latitude: coordinate.latitude, longitude: coordinate.longitude)
-    }
-}
-
-struct Coordinate: Codable {
     var latitude: Double
     var longitude: Double
+
 }
+
+
