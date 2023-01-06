cc4d6cd63200c800a5afebed64614471429d4763
thwong221 <tszhim.wong1010@yahoo.com>
2023-01-06 20:15:29 +0800

    Add columns to grasp more info of paper lanterns shops

--- lanternSwiftUI/Model/AnnotatedItem.swift
+++ lanternSwiftUI/Model/AnnotatedItem.swift
@@ -11,9 +11,12 @@ import Foundation
 struct AnnotatedItem : Decodable, Identifiable {
     var id: Int
     let name : String
-    var description: String
     var address: String
+    var description: String
     var district: String
+    var website: String
+    var telephone: Int
+    var email: String
     var latitude: Double
     var longitude: Double
 
