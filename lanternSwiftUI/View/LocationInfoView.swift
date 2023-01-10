e2101e9bea3c535d3fcc5176b42aa920dabd43c4
thwong221 <tszhim.wong1010@yahoo.com>
2023-01-11 00:11:33 +0800

    Modified the variables for easily identifying and Image component changed as AsyncImage to get the images from Firebase easily

--- lanternSwiftUI/View/LocationInfoView.swift
+++ lanternSwiftUI/View/LocationInfoView.swift
@@ -1,38 +1,156 @@
 //
-//  LocationInfoView.swift
+//  LocationInfoViewFB.swift
 //  lanternSwiftUI
 //
-//  Created by WONG TSZ HIM on 5/1/2023.
+//  Created by WONG TSZ HIM on 7/1/2023.
 //
 
 import SwiftUI
 
-
 struct LocationInfoView: View {
-    var locations: AnnotatedItem
+    var place: PlaceCol
+    
+    @StateObject var captionViewModel = ImageCaptionGetDataViewModel()
     
     @State private var isShareSheetDisplay = false
+    @State private var isShownPostView = false
     
+    @State var imageURLList = [String]()
+    //    @State var shopID: String
+    
+    private func captionRowView(caption: ImageCaption) -> some View {
+        VStack(alignment: .leading) {
+            
+            AsyncImage(url: URL(string: caption.imageURL)) { image in
+                image
+                    .resizable()
+                    .scaledToFit()
+            } placeholder: {
+                Color.purple.opacity(0)
+            }
+            .frame(width: 300, height: 200)
+            .padding()
+            
+            Text(caption.caption).padding()
+            Divider()
+        }
+    }
     
     var body: some View {
-        let url = URL(string: locations.website)!
-        let message = "\n\nHere are the information of Paper Lantern Shop:\nName: \(locations.name) \nAddress: \(locations.address) \nPhone: \(locations.telephone) \nEmail: \(locations.email)\n \nCheck it out!"
+        let url = URL(string: place.website)!
+        let phone = URL(string: "tel:852\(place.telephone)")!
+        let email = URL(string: "mailto:\(place.email)")!
+        
+        let message = "\n\nHere are the information of Paper Lantern Shop:\nName: \(place.name) \nAddress: \(place.address) \nPhone: \(place.telephone) \nEmail: \(place.email)\n \nCheck it out!"
         
         ScrollView {
             VStack(alignment: .leading) {
-                Text(locations.name)
-                    .font(.title)
-                    .bold()
-                    .padding()
-
-                // Button to share / bookmark / website
-
-                HStack {
+                Group {
+                    HStack {
+                        AsyncImage(url: URL(string: place.mainImgURL)) { image in
+                            image
+                                .resizable()
+                                .scaledToFill()
+                        } placeholder: {
+                            Color.red.opacity(0)
+                        }
+                        .frame(width: 110, height: 110)
+                        .clipped()
+                        .clipShape(RoundedRectangle(cornerRadius: 8.0))
+                        .padding()
+                        
+                        Text(place.name)
+                            .font(.title)
+                            .bold()
+                            .padding()
+                    }
+                }
+                
+                Group {
+                    // Information
+                    
+                    HStack {
+                        Image(systemName: "location.fill").font(.title3)
+                        Text(place.address)
+                    }.font(.subheadline)
+                        .padding()
+                    
+                    HStack {
+                        Link(destination: phone) {
+                            Image(systemName: "phone.fill").font(.title3)
+                            Text("\(place.telephone)")
+                            Spacer()
+                            Image(systemName: "chevron.right")
+                        }
+                        .foregroundColor(.black)
+                    }.font(.subheadline)
+                        .padding()
+                    
+                    HStack {
+                        Link(destination: email) {
+                            Image(systemName: "envelope.fill").font(.title3)
+                            Text(place.email)
+                            Spacer()
+                            Image(systemName: "chevron.right")
+                        }
+                    }.font(.subheadline)
+                        .foregroundColor(.black)
+                        .padding()
+                }
+                
+                Divider()
+                
+                Group {
+                    Text("About \(place.name)")
+                        .font(.title2).bold().padding()
+                    Text(place.description).padding()
+                }
+                
+                Group {
+                    //Image Captions for the shop
+                    
+                    VStack(alignment: .leading) {
+                        Divider()
+                        
+                        
+                        ForEach(0 ..< captionViewModel.caption.count, id: \.self) {
+                            AsyncImage(url: URL(string: captionViewModel.caption[$0].imageURL)) { image in
+                                image
+                                    .resizable()
+                                    .scaledToFit()
+                            } placeholder: {
+                                Color.purple.opacity(0)
+                            }
+                            .frame(width: 300, height: 200)
+                            .padding()
+                            
+                            Text(captionViewModel.caption[$0].caption).padding()
+                            Divider()
+                        }
+                    }.onAppear(){
+                        self.captionViewModel.fetchCaptions(shopID: place.id)
+                        print("Data are appeared.")
+                    }
+                    .onDisappear(){
+                        print("Data are disappeared.")
+                    }
+                }
+                
+            }.padding()
+                .navigationTitle(place.name)
+                .navigationBarTitleDisplayMode(.inline)
+                .navigationBarItems(trailing:
+                                        HStack {
+                    
+                    Link(destination: url) {
+                        Image(systemName: "link")
+                    }
+                    
+                    
                     if #available(iOS 16.0, *) {
                         ShareLink(item: url, subject: Text("Check It Out!"), message:Text(message)) {
                             Image(systemName: "square.and.arrow.up")
-                        }.font(.title2).padding()
-
+                        }
                     } else {
                         // Fallback on earlier versions
                         Button(action: {
@@ -45,55 +163,26 @@ struct LocationInfoView: View {
                                                              completion: nil)
                             
                         }) {
-                            Image(systemName: "square.and.arrow.up").font(.title2).padding()
+                            Image(systemName: "square.and.arrow.up")
                         }
                     }
                     
+                    
                     Button(action: {
-                        Text("Button")
+                        self.isShownPostView = true
                     }) {
-                        Image(systemName: "bookmark.fill")
-                    }.font(.title2).padding()
+                        Image(systemName: "square.and.pencil")
+                    }.onTapGesture {
+                        self.isShownPostView = true
+                    }
+                    .sheet(isPresented: $isShownPostView) {
+                        PostCaptionView(shopID: place.id)
+                    }
+                    
                     
                 }
-                
-                Divider()
-                
-                // Information
-                
-                HStack {
-                    Image(systemName: "location.fill").font(.title3)
-                    Text(locations.address)
-                }.font(.subheadline)
-                    .padding()
-                
-                HStack {
-                    Image(systemName: "phone.fill").font(.title3)
-                    Text("\(locations.telephone)")
-                }.font(.subheadline)
-                    .padding()
-                
-                
-                HStack {
-                    Image(systemName: "envelope.fill").font(.title3)
-                    Text(locations.email)
-                }.font(.subheadline)
-                    .padding()
-                
-                
-                Divider()
-                
-                Text("About \(locations.name)")
-                    .font(.title2).bold().padding()
-                
-                
-                Text(locations.description).padding()
-                
-                
-                
-            }.padding()
-                .navigationTitle(locations.name)
-                .navigationBarTitleDisplayMode(.inline)
+                                    
+                )
         }
         
         
@@ -102,6 +191,7 @@ struct LocationInfoView: View {
 
 struct LocationInfoView_Previews: PreviewProvider {
     static var previews: some View {
-        LocationInfoView(locations: lanternLocation[1])
+        let place = PlaceCol(id: "1000", name: "Paper Lanterns Places", address: "Flat 5, 10/F, Kam Shing Building, 12 Wang Hoi Road, Kowloon Bay", description: "Paper Lanterns Places", district: "Kowloon Bay", website: "https://web.whatsapp.com", telephone: "98674321", email: "abc@gmail.com", latitude: 22.322946, longitude: 114.21071, mainImgURL: "google.com")
+        LocationInfoView(place: place)
     }
 }
