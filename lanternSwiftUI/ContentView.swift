ce6436c22d56d4d6e367d6e162aacb73625c67ac
pthwong <tszhim.wong1010@yahoo.com>
2023-01-11 15:09:43 +0800

    Navigation View Style for iPad & add the preview for checking the

--- lanternSwiftUI/ContentView.swift
+++ lanternSwiftUI/ContentView.swift
@@ -16,12 +16,17 @@ struct ContentView: View {
             MapView().tabItem({
                 Image(systemName: "map")
             })
-        }
+        }.navigationViewStyle(.stack)
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+            .previewDevice(PreviewDevice(rawValue: "iPhone 14 Pro"))
+            .previewDisplayName("iPhone 14 Pro")
+        ContentView()
+            .previewDevice(PreviewDevice(rawValue: "iPad Pro (11-inch) (4th generation)"))
+            .previewDisplayName("iPad Pro (11-inch) (4th generation)")
     }
 }
